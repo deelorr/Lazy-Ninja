cfb0c43301673d987b9e1116938e225483a3bc65
deelorr <deelorr@gmail.com>
2024-10-25 13:19:58 -0700

    updated store owner.gd and removed store open from global

--- scripts/characters/npc/store_owner.gd
+++ scripts/characters/npc/store_owner.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 
-@onready var store_gui = $"../CanvasLayer/StoreInventoryGUI"
+@onready var store_gui: Control = $"../CanvasLayer/StoreInventoryGUI"
+@onready var store_owner_dialogue: Resource = preload("res://dialogue/store_owner.dialogue")
+@onready var animations: AnimationPlayer = $AnimationPlayer
 
 var store_open: bool = false
+var in_shop_range: bool = false
 
 func _physics_process(_delta: float) -> void:
-	if Global.in_shop_range == true and Input.is_action_just_pressed("action"):
-		DialogueManager.show_dialogue_balloon(load("res://dialogue/store_owner.dialogue"), "start")
+	if in_shop_range == true and Input.is_action_just_pressed("action"):
+		DialogueManager.show_dialogue_balloon(store_owner_dialogue, "start")
 		toggle_store()
 
-func toggle_store():
+func toggle_store() -> void:
 	if store_open:
 		store_gui.close()
 		store_open = false
@@ -17,13 +20,14 @@ func toggle_store():
 		store_gui.open()
 		store_open = true
 
-func _on_area_2d_body_entered(body) -> void:
-	if body is CharacterBody2D:
-		Global.in_shop_range = true
-		$AnimationPlayer.play("bubble_pop_up")
+func _on_area_2d_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		in_shop_range = true
+		animations.play("bubble_pop_up")
 
-func _on_area_2d_body_exited(body) -> void:
-	if body is CharacterBody2D:
-		Global.in_shop_range = false
-		store_open = false
-		$AnimationPlayer.play("bubble_pop_down")
+func _on_area_2d_body_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		if store_open:
+			toggle_store()
+			in_shop_range = false
+			animations.play("bubble_pop_down")
--- scripts/global.gd
+++ scripts/global.gd
@@ -3,6 +3,5 @@ class_name EventBus
 
 signal enemy_killed(enemy_type)
 
-var in_shop_range: bool = false
 var slime_count: int = 0
 var max_slimes: int = 15
