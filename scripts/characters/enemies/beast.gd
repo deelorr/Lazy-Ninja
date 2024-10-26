b0739731e812f8d6df87a34a261efc9b086ec73a
deelorr <deelorr@gmail.com>
2024-10-26 07:48:15 -0700

    added chase logic to beast monster

---
+++ scripts/characters/enemies/beast.gd
@@ -0,0 +1,78 @@
+extends CharacterBody2D
+
+@export var speed: int = 50
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var direction_timer: Timer = $direction_timer
+
+var is_dead: bool = false
+var is_chasing: bool = false
+var player: Player = null
+
+func _ready() -> void:
+	player = SceneManager.player
+	velocity = Vector2.DOWN * speed
+	direction_timer.start()
+
+func _physics_process(_delta: float) -> void:
+	if is_dead:
+		return
+
+	if is_chasing and player:
+		# Chase player if in chase mode
+		chase_player()
+	else:
+		move_and_slide()  # Random movement
+
+	update_animation()
+
+func update_animation() -> void:
+	if velocity.length() == 0:
+		if animation_player.is_playing():
+			animation_player.stop()
+		return
+	
+	animation_player.play("walk_" + get_direction())
+
+func get_direction() -> String:
+	if abs(velocity.x) > abs(velocity.y):
+		return "left" if velocity.x < 0 else "right"
+	else:
+		return "up" if velocity.y < 0 else "down"
+
+func _on_hurt_box_area_entered(area: Area2D) -> void:
+	if not area.is_in_group("weapon"):
+		return
+
+	$hit_box.set_deferred("monitorable", false)  # Prevents further collision detection
+	is_dead = true
+	animation_player.play("death")
+
+	await animation_player.animation_finished  # Wait for the death animation to finish
+	Global.enemy_killed.emit("slime")
+	Global.slime_count -= 1
+	print("slime destroyed", Global.slime_count, "/", Global.max_slimes)
+	SceneManager.player.add_xp(5)
+	queue_free()  # Remove the enemy
+
+func _on_direction_timer_timeout() -> void:
+	if not is_chasing:
+		var angle = randf() * 2 * PI  # Random angle for random movement
+		var new_direction = Vector2(cos(angle), sin(angle)).normalized()
+		velocity = new_direction * speed
+
+	direction_timer.start()  # Restart the timer
+
+func chase_player() -> void:
+	# Chase player
+	var direction = (player.position - position).normalized()
+	velocity = direction * speed
+	move_and_slide()
+
+func _on_detection_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		is_chasing = true  # Set to chase mode
+
+func _on_detection_body_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		is_chasing = false  # Stop chasing when player exits detection
