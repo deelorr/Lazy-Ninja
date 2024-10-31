c344e9ce7cfad7f818aa9041b64c86b2180cb538
deelorr <deelorr@gmail.com>
2024-10-31 14:03:42 -0700

    just revising scripts, going to attempt base scene scene mamager next... gulp

---
+++ scripts/gui/hud/quest_panel.gd
@@ -0,0 +1,36 @@
+extends NinePatchRect
+
+@onready var quest_title: Label = $Quests/HBoxContainer/quest_title
+@onready var quest_objective: Label = $Quests/quest_details/quest_objective
+@onready var quest_progress: ProgressBar = $Quests/quest_details/ProgressBar
+@onready var player: Player = SceneManager.player
+
+func _ready():
+	if QuestManager:
+		QuestManager.connect("current_objective_changed", Callable(self, "_on_current_objective_changed"))
+	else:
+		print_debug("Error: QuestManager node not found.")
+	self.visible = false
+	
+func _process(_delta):
+	if player:
+		update_quest_panel()
+		
+func update_quest_panel():
+	quest_progress.visible = false
+	if QuestManager.active_quests.size() > 0:
+		self.visible = true
+		var quest = QuestManager.active_quests.values()[0]
+		quest_title.text = quest.title
+		if QuestManager.current_objective_index < quest.objectives.size():
+			var objective = quest.objectives[QuestManager.current_objective_index]
+			quest_objective.text = objective.description
+			quest_progress.visible = true
+			quest_progress.value = objective.current_count
+			quest_progress.max_value = objective.target_count
+		else:
+			self.visible = false
+			
+func _on_current_objective_changed(quest_id, description):
+	print_debug("Received current_objective_changed signal for Quest ID %d: %s" % [quest_id, description])
+	quest_objective.text = description
--- scripts/gui/hud/stat_panel.gd
+++ scripts/gui/hud/stat_panel.gd
@@ -1,51 +1,20 @@
 extends NinePatchRect
 
 @onready var health_bar: ProgressBar = $Stats/Health/Label2/ProgressBar
-@onready var quest_title: Label = $QuestPanel/Quests/HBoxContainer/quest_title
-@onready var quest_objective: Label = $QuestPanel/Quests/quest_details/quest_objective
-@onready var quest_progress: ProgressBar = $QuestPanel/Quests/quest_details/ProgressBar
-@onready var quest_panel: NinePatchRect = $QuestPanel
 @onready var level_label: Label = $Stats/Level/level_label
-
-var player: Player = null
-
-func _ready():
-	player = SceneManager.player
-	if QuestManager:
-		QuestManager.connect("current_objective_changed", Callable(self, "_on_current_objective_changed"))
-	else:
-		print_debug("Error: QuestManager node not found.")
-	quest_panel.visible = false
+@onready var level_progress: ProgressBar = $Stats/Level/Label/ProgressBar
+@onready var player: Player = SceneManager.player
 
 func _process(_delta):
 	if player:
-		update_quest_panel()
-		update_level()
+		update_level_progress(player.current_xp, player.xp_for_next_level)
 		update_health(player.current_health, player.max_health)
 
 func update_health(health: int, max_health: int):
 	health_bar.value = health
 	health_bar.max_value = max_health
 
-func update_quest_panel():
-	quest_progress.visible = false
-	if QuestManager.active_quests.size() > 0:
-		quest_panel.visible = true
-		var quest = QuestManager.active_quests.values()[0]
-		quest_title.text = quest.title
-		if QuestManager.current_objective_index < quest.objectives.size():
-			var objective = quest.objectives[QuestManager.current_objective_index]
-			quest_objective.text = objective.description
-			quest_progress.visible = true
-			quest_progress.value = objective.current_count
-			quest_progress.max_value = objective.target_count
-		else:
-			quest_panel.visible = false
-
-func update_level():
-	if player:
-		level_label.text = str(player.current_level)
-
-func _on_current_objective_changed(quest_id, description):
-	print_debug("Received current_objective_changed signal for Quest ID %d: %s" % [quest_id, description])
-	quest_objective.text = description
+func update_level_progress(current_xp: int, xp_for_next_level: int):
+	level_progress.value = current_xp
+	level_progress.max_value = xp_for_next_level
+	level_label.text = str(player.current_level)
