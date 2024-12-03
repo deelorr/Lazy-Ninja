faeb8f26ff42c8152f5c6cc3b0f7b4fe490481d7
deelorr <deelorr@gmail.com>
2024-12-02 18:07:41 -0800

    added animations for the boss slime

--- scripts/characters/enemies/boss_slime.gd
+++ scripts/characters/enemies/boss_slime.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var direction_timer: Timer = $direction_timer
 
+var speed: int = 45
 var is_dead: bool = false
-var speed: int = 50
 
 func _ready() -> void:
 	velocity = Vector2.DOWN * speed
@@ -21,7 +21,7 @@ func update_animation() -> void:
 		if animation_player.is_playing():
 			animation_player.stop()
 		return
-	animation_player.play("walk_" + get_direction())
+	animation_player.play("idle")
 
 func get_direction():
 	if abs(velocity.x) > abs(velocity.y):
@@ -36,10 +36,9 @@ func _on_hurt_box_area_entered(area: Area2D) -> void:
 	is_dead = true
 	animation_player.play("death")
 	await animation_player.animation_finished
-	Global.enemy_killed.emit("slime")
-	Global.slime_count -= 1
-	print("slime destroyed", Global.slime_count, "/", Global.max_slimes)
-	SceneManager.player.add_xp(5)
+	Global.enemy_killed.emit("boss_slime")
+	print("boss slime destroyed")
+	SceneManager.player.add_xp(50)
 	queue_free()
 
 func _on_direction_timer_timeout() -> void:
